0afa45139310c7480499f392ec3bf09121e12091
simonecapezzani <simonecapezzani@gmail.com>
2024-05-21 09:55:15 +0200

    Adding authentication slice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { useSelector } from 'react-redux';
+
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
 
 
+
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
       <Counter />
     </>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Header.module.css';
+import { authActions } from '../store';
 
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  const logoutHandler = (event)=>{
+    event.preventDefault();
+
+    dispatch(authActions.logout());
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
+        {isAuth && <ul>
           <li>
             <a href='/'>My Products</a>
           </li>
@@ -13,9 +26,10 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
-        </ul>
+        </ul>}
+        
       </nav>
     </header>
   );
